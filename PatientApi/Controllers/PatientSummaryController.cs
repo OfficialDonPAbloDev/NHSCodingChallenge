bfab3c4ce245734a315dca67bea2ff851c64c3ee
PAblo <paul1972moores@hotmail.co.uk>
2026-04-14 11:20:19 +0100

    Initial project creation

---
+++ PatientApi/Controllers/PatientSummaryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatientApi.Controllers
+{
+    [AllowAnonymous]
+    public class PatientSummaryController : Controller
+    {
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok("Ok");
+        }
+    }
+}
